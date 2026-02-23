73c4175f479decb46440dc45bec618bcbcd9d659
LeonardoFioravanti <160791947+LeonardoFioravanti@users.noreply.github.com>
2026-02-23 01:34:55 -0600

    Login commit

---
+++ Gestion-de-recursos-para-PYMES/Gestion-de-recursos-para-PYMES/Data/ApplicationDbContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Gestion_de_recursos_para_PYMES.Data
+{
+    public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext(options)
+    {
+    }
+}
